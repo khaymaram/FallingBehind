8e0a182e7ff59a5bbdb5e14458281812f337e0af
Khaymar Moe <kamoe25@terpmail.umd.edu>
2025-11-15 18:16:27 -0500

    nav bar functionality

--- functions.js
+++ functions.js
@@ -1,6 +1,33 @@
 // navigate to walks screen when start button is clicked
 const start = document.getElementById('start');
-function nagivateToWalkScreen() {
+
+
+function navigateToWalkScreen() {
     window.location.href = 'walkscreen.html';
 }
-start.addEventListener('click', nagivateToWalkScreen);
+
+function navigateToRecsScreen() {
+    window.location.href = 'walkrecs.html';
+}
+
+function navigateToProfileScreen() {
+    window.location.href = 'profile.html';
+}
+
+if (start) {
+    start.addEventListener('click', navigateToWalkScreen);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const mywalks = document.getElementById('mywalks-tab');
+    const walkrecs = document.getElementById('walkrecs-tab');
+    const profile = document.getElementById('profile-tab');
+
+    mywalks.addEventListener('click', navigateToWalkScreen);
+    walkrecs.addEventListener('click', navigateToRecsScreen);
+    profile.addEventListener('click', navigateToProfileScreen);
+});
+
+
+
+
