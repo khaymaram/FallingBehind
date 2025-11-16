6c2fa6ef7285e295a7ea63941d77de73e6b91773
Khaymar Moe <kamoe25@terpmail.umd.edu>
2025-11-15 19:36:31 -0500

    created popup feature

--- functions.js
+++ functions.js
@@ -1,6 +1,19 @@
 // navigate to walks screen when start button is clicked
 const start = document.getElementById('start');
+const popup = document.getElementById('start-popup');
+const popupBtn = document.getElementById('popup-button');
+let username = "";
 
+function setUsername(){
+    const input = document.getElementById('enter-name').value;
+    username = input;
+    updateName();
+}
+
+function updateName(){
+    document.getElementById('profile-name').innerText = 
+        `${username}`;
+}
 
 function navigateToWalkScreen() {
     window.location.href = 'walkscreen.html';
@@ -14,19 +27,28 @@ function navigateToProfileScreen() {
     window.location.href = 'profile.html';
 }
 
-if (start) {
-    start.addEventListener('click', navigateToWalkScreen);
-}
-
 document.addEventListener("DOMContentLoaded", () => {
     const mywalks = document.getElementById('mywalks-tab');
     const walkrecs = document.getElementById('walkrecs-tab');
     const profile = document.getElementById('profile-tab');
 
-    mywalks.addEventListener('click', navigateToWalkScreen);
-    walkrecs.addEventListener('click', navigateToRecsScreen);
-    profile.addEventListener('click', navigateToProfileScreen);
+    if (mywalks) mywalks.addEventListener('click', navigateToWalkScreen);
+    if (walkrecs) walkrecs.addEventListener('click', navigateToRecsScreen);
+    if (profile) profile.addEventListener('click', navigateToProfileScreen);
 });
+
+function openStartPopup(){
+    popup.style.display = 'flex';
+}
+
+if (start) {
+    start.addEventListener('click', openStartPopup);
+}
+
+popupBtn.addEventListener('click', navigateToWalkScreen);
+
+
+
 
 
 
